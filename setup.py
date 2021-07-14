d6b3eee2726cd9dab528249e113405d2b1485f95
Edwin Lee <leeed2001@gmail.com>
2021-07-13 19:11:11 -0500

    Add GHA and other Python structure stuff

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="EnergyPlus Parser-Replacer",
+    version="0.1",
+    description="A developer tool for EnergyPlus, and maybe beyond, for parsing and replacing function args",
+    url='https://energyplus.net',
+    license='Something'
+)
